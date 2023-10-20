d3e12a911d6edeb6c32d004f77c9b17520493790
Kristijan Stošić <nimeria83@yahoo.com>
2023-10-20 09:32:53 +0200

    Preparing search and pagination for users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -68,7 +68,30 @@ const updateUserProfile = async (req, res) => {
 // @route GET /api/users
 // @access Private/Admin
 const getUsers = async (req, res) => {
-    res.send('get users')
+    const pageSize = 8
+    const page = Number(req.query.page) || 1
+    const limit = pageSize
+    const skip = (page - 1) * pageSize
+
+    const keyword = req.query.keyword ? {
+        email: {
+            $regex: req.query.keyword,
+            $options: 'i',
+        },
+    }
+        : {}
+
+    const count = await User.countDocuments({ ...keyword })
+    const users = await User.find({ ...keyword })
+        .limit(limit)
+        .skip(skip)
+
+    if (!users?.length) {
+        res.status(404)
+        throw new Error('Users not found!')
+    }
+
+    res.status(200).json({ users, page, pages: Math.ceil(count / pageSize) })
 }
 
 // @desc Get user by ID
